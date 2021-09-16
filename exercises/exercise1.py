254f8701ad5dba0eb2d60ee0d9a3d44caedb81d1
AdrianPlesner <adrian.plesner@gmail.com>
2021-09-16 22:00:12 +0200

    exercise1 solution

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,7 +1,8 @@
 from emulators.Device import Device
 from emulators.Medium import Medium
 from emulators.MessageStub import MessageStub
-
+import random
+import time
 
 class GossipMessage(MessageStub):
 
@@ -14,6 +15,16 @@ class GossipMessage(MessageStub):
         return f'{self.source} -> {self.destination} : {self.secrets}'
 
 
+class CallMessage(MessageStub):
+
+    def __init__(self, sender: int, destination: int, nr: int):
+        super().__init__(sender, destination)
+        self.nr = nr
+
+    def __str__(self):
+        return f'{self.source} -> {self.destination} nr: {self.nr}'
+
+
 class Gossip(Device):
 
     def __init__(self, index: int, number_of_devices: int, medium: Medium):
@@ -22,11 +33,49 @@ class Gossip(Device):
         # or sharing of all the public keys in a cryptographic system
         self._secrets = set([index])
 
+    def get_calle(self):
+        res = self.index()
+        while res == self.index():
+            res = random.randint(0, self.number_of_devices()-1)
+        return res
+
     def run(self):
         # the following is your termination condition, but where should it be placed?
-        # if len(self._secrets) == self.number_of_devices():
-        #    return
-        return
+        while True:
+            ingoing = self.medium().receive()
+            if ingoing is None:
+                call_to = self.get_calle()
+                self.medium().send(CallMessage(self.index(), call_to, 0))
+                wait = time.time()
+                rand_tim = random.random() * 1.5
+                while time.time() - wait < rand_tim and (ingoing is None or ingoing.source is not call_to):
+                    ingoing = self.medium().receive()
+                if ingoing is not None and type(ingoing) is CallMessage and ingoing.nr is 1:
+                    self.medium().send(GossipMessage(self.index(), call_to, self._secrets))
+                    ingoing = self.medium().receive()
+                    while ingoing is None or ingoing.source is not call_to:
+                        ingoing = self.medium().receive()
+                    if type(ingoing) is GossipMessage:
+                        for sec in ingoing.secrets:
+                            if sec not in self._secrets:
+                                self._secrets.add(sec)
+                else:
+                    self.medium().send(CallMessage(self.index(), call_to, -1))
+            elif type(ingoing) is CallMessage and ingoing.nr is 0:
+                caller = ingoing.source
+                self.medium().send(CallMessage(self.index(), caller, 1))
+                ingoing = self.medium().receive()
+                while ingoing is None or ingoing.source is not caller:
+                    ingoing = self.medium().receive()
+                if type(ingoing) is GossipMessage:
+                    for sec in ingoing.secrets:
+                        if sec not in self._secrets:
+                            self._secrets.add(sec)
+                    self.medium().send(GossipMessage(self.index(), caller, self._secrets))
+
+            if len(self._secrets) == self.number_of_devices():
+                return
 
     def print_result(self):
         print(f'\tDevice {self.index()} got secrets: {self._secrets}')
+
